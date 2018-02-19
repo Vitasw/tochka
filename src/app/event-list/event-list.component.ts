27d0c7a343457afa0a9ba8066925ab092e9d7de4
Vita <vitasw@gmail.com>
2018-02-20 01:03:01 +0500

    Фильтр по типам событий

--- src/app/event-list/event-list.component.ts
+++ src/app/event-list/event-list.component.ts
@@ -1,18 +1,34 @@
-import { Component, OnInit } from '@angular/core';
-import { EVENTS } from '../mock-data';
-import { Event } from '../event';
+import {Component, OnInit} from '@angular/core';
+import {EVENTS} from '../mock-data';
+import {Event} from '../event';
+import {EventType} from '../event-type.enum';
 
-@Component({selector: 'app-event-list',
-  templateUrl: './event-list.component.html',
-  styleUrls: ['./event-list.component.css']
+@Component({
+    selector: 'app-event-list',
+    templateUrl: './event-list.component.html',
+    styleUrls: ['./event-list.component.css']
 })
 export class EventListComponent implements OnInit {
 
-  events: Event[] = EVENTS;
+    events: Event[] = EVENTS;
 
-  constructor() { }
+    selectedTypeFilter: string;
 
-  ngOnInit() {
-  }
+    filterEvents = Object.keys(EventType);
+
+    constructor() {
+    }
+
+    ngOnInit() {
+    }
+
+    selectEventType(eventType) {
+        this.selectedTypeFilter = eventType;
+        this.events.sort((a,b) => {
+            if (a.type === this.selectedTypeFilter) {
+                return 1;
+            }
+        });
+    }
 
 }
